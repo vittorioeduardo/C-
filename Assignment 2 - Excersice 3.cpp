b35daf6d57b16361419e6deacdd3d33cd119dabf
vittorioeduardo <50897165+vittorioeduardo@users.noreply.github.com>
2020-12-28 23:21:46 -0600

    Update Assignment 2 - Excersice 3.cpp

--- Assignment 2 - Excersice 3.cpp
+++ Assignment 2 - Excersice 3.cpp
@@ -1,3 +1,17 @@
+/*
+Write a program that translates a letter grade into a number grade. Letter grades are A, B, C, D, and F, possibly followed by + or –. Their numeric values are 4, 3, 2, 1, and 0. There is no F+ or F–. 
+A + increases the numeric value by 0.3, a – decreases it by 0.3. However, an A+ has value 4.0. 
+
+Sample Outputs from Program:
+
+RUN#1:
+Enter a letter grade: B- 
+The numeric value is 2.7
+
+RUN#2:
+Enter your letter grade: B+
+The equivalent number grade is 3.3
+*/
 #include <iostream>
 #include <string>
 #include <algorithm> // Transform all inputs to UPPERCASE
