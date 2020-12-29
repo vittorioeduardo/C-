f74541ad59345ea1e0f6365d9c4017f8912d1f9f
vittorioeduardo <50897165+vittorioeduardo@users.noreply.github.com>
2020-12-28 23:17:12 -0600

    Update Assignment 2 - Excersice 2.cpp

--- Assignment 2 - Excersice 2.cpp
+++ Assignment 2 - Excersice 2.cpp
@@ -1,3 +1,20 @@
+/*
+Write a program that inputs the amount of the purchase and calculates the shipping charge based on the following table:
+$0.00 - $250.00: $5.00
+$250.01 - $500.00: $8.00
+$500.01 - $1,000.00: $10.00
+$1,000.01 - $5,000.00: $15.00
+over $5,000.00: $20.00
+
+Sample Output from Program:
+
+RUN#1:
+     Enter a purchase amount to find out your shipping charges.
+
+     Please enter the amount of your purchase: 234.65
+     The shipping charge on a purchase of $234.65 is $5.00.
+
+*/
 #include <iostream>
 using namespace std;
 int main() {
