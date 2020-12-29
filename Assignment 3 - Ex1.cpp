b226810d24ad9bf903505571505af57d3be55011
vittorioeduardo <50897165+vittorioeduardo@users.noreply.github.com>
2020-12-28 23:28:35 -0600

    Update Assignment 3 - Ex1.cpp

--- Assignment 3 - Ex1.cpp
+++ Assignment 3 - Ex1.cpp
@@ -1,3 +1,22 @@
+/*
+A year with 366 days is called a leap year. A year is a leap year if it is divisible by four (for example, 1980), except that it is not a leap year if it is divisible by 100 (for example, 1900); however, it is a leap year if it is divisible by 400 (for example, 2000). There were no exceptions before the introduction of the Gregorian calendar on October 15, 1582 (1500 was a leap year). 
+
+Write a program that asks the user for a year and computes whether that year is a leap year. 
+
+Here are a few run results:
+
+Please enter a year: 2015
+2015 is not a leap year.
+Press any key to continue . . .
+
+Please enter a year: 2016
+2016 is a leap year.
+Press any key to continue . . .
+
+Please enter a year: 1900
+1900 is not a leap year.
+Press any key to continue . . .
+*/
 #include<iostream>
 using namespace std;
 int main() {
